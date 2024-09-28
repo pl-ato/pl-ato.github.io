8b6509e2e43d2c7eac180a27792169a6e7b4e0ee
Silverster98 <1539168414@qq.com>
2024-09-28 08:32:31 +0800

    Update

---
+++ static/js/carousel.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Check for click events on the navbar burger icon
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 100000,
+  }
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+});
